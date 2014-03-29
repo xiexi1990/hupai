1b6741328adb7082eab396a5b347896f81050abe
xiexi1990 <xiexi_1990@126.com>
2014-03-29 15:10:23 +0800

    improve div rect functions

--- DivRectAndDrawingFuncs.cpp
+++ DivRectAndDrawingFuncs.cpp
@@ -31,62 +31,18 @@ int MainWnd::DivWhole(const CRect &whole, int num, RECTARR &rectlst)
 int MainWnd::DivPlayerRect(const CRect &playerrect, RECTARR &rectlst)
 {
 	rectlst.clear();
-	const double 
-		
-		
-		vline_nameleft = 0, hline_nametop = 0.1, vline_nameright = 1-vline_nameleft, hline_namebottom = 0.45,
-		vline_menleft = 0.3, hline_mentop = 0.02, vline_menright = 1-vline_menleft, hline_menbottom = 0.15,
-		vline_mingleft = 0.02, hline_mingtop = 0.05, vline_mingright = 0.25, hline_mingbottom = 0.6,
-		vline_anleft = 1-vline_mingright, hline_antop = hline_mingtop, vline_anright = 1-vline_mingleft, hline_anbottom = hline_mingbottom, 
-		vline_fanleft = 0, hline_fantop = 0.45, vline_fanright = 1-vline_fanleft, hline_fanbottom = 0.75,
-		vline_huleft = 0.1, hline_hutop = 0.75, vline_huright = 1-vline_huleft, hline_hubottom = 1;	
-
-	CRect _stock;
-	CRect b;
-
-#define OFFSET b.OffsetRect(playerrect.TopLeft())
-#define SETRECT(x) b.left = W * vline_##x##left;b.right = W * vline_##x##right;b.top = H * hline_##x##top;b.bottom = H * hline_##x##bottom;OFFSET;
-#define SETRECTPUSH(x) SETRECT(x) rectlst.push_back(b);
-	
+	CRect ming, an;
 
 	rectlst.push_back(_setRect(playerrect,        0,      0.1,        1,     0.45)); //name
 	rectlst.push_back(_setRect(playerrect,        0,     0.45,        1,     0.75)); //fan
 	rectlst.push_back(_setRect(playerrect,      0.3,     0.02,      0.7,     0.15)); //men
 	rectlst.push_back(_setRect(playerrect,      0.1,     0.75,      0.9,     0.45)); //hu
-	rectlst.push_back(_setRect(playerrect,     0.02,     0.05,     0.25,      0.6)); //ming
+	rectlst.push_back(ming = _setRect(playerrect,     0.02,     0.05,     0.25,      0.6)); //ming
 	rectlst.push_back(_setRect(ming,              0,        0,        1,      0.5)); //ming word
 	rectlst.push_back(_setRect(ming,              0,      0.5,        1,        1)); //ming cnt
-	rectlst.push_back(_setRect(playerrect,     0.75,     0.05,     0.98,      0.6)); //an
+	rectlst.push_back(an = _setRect(playerrect,     0.75,     0.05,     0.98,      0.6)); //an
 	rectlst.push_back(_setRect(an,                0,        0,        1,      0.5)); //an word
 	rectlst.push_back(_setRect(an,                0,      0.5,        1,        1)); //an cnt
-
-	SETRECTPUSH(name)
-	SETRECTPUSH(fan)
-	SETRECTPUSH(men)
-	SETRECTPUSH(hu)
-	SETRECTPUSH(ming)
-
-	///ming word
-	_stock = b;
-	b.bottom -= _stock.Height() / 2;
-	rectlst.push_back(b);
-
-	///ming cnt
-	b = _stock;
-	b.top += _stock.Height() / 2;
-	rectlst.push_back(b);
-
-	SETRECTPUSH(an)
-	
-	///an word
-	_stock = b;
-	b.bottom -= _stock.Height() / 2;
-	rectlst.push_back(b);
-
-	///an cnt
-	b = _stock;
-	b.top += _stock.Height() / 2;
-	rectlst.push_back(b);
 
 	return 0;
 }
